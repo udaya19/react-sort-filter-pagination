ded6b8b4e16f519edfa070955e3fe90dfc3e32b3
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2023-04-05 10:55:59 +0530

    proptypes to pagination and filter ui

---
+++ src/components/common/listGroup.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+const ListGroup = (props) => {
+  const { items, textProperty, valueProperty } = props;
+  return (
+    <ul class="list-group">
+      {items.map((item) => (
+        <li key={item[valueProperty]} class="list-group-item">
+          {item[textProperty]}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ListGroup;
--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import _ from "lodash";
+import PropTypes from "prop-types";
 const Pagination = (props) => {
   const { itemsCount, pageSize, onPageChange, currentPage } = props;
   console.log(currentPage);
@@ -22,6 +23,13 @@ const Pagination = (props) => {
       </ul>
     </nav>
   );
+};
+
+Pagination.propTypes = {
+  itemsCount: PropTypes.number.isRequired,
+  pageSize: PropTypes.number.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
 };
 
 export default Pagination;
